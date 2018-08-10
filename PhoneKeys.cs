df6e18031907f2163ff295a11e82b48c20211c41
jitingogia06 <42280447+jitingogia06@users.noreply.github.com>
2018-08-10 19:46:33 +0100

    Add files via upload

---
+++ PhoneKeys.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GAS.TestCode
+{
+    public class PhoneKeys
+    {
+        /// <summary>
+        /// Returns dictionary of phone keys
+        /// </summary>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetPhoneKeys()
+        {
+            var contentxml = new XmlDocument();
+            contentxml.Load(@"../../PhoneKeys.xml");
+            
+            var data = XElement.Parse(contentxml.InnerXml)
+            .Elements("key")
+            .ToDictionary(
+                el => (string)el.Attribute("number"),
+                el => (string)el.Attribute("text")
+            );
+            
+            return data;
+        }
+
+        /// <summary>
+        /// Processes a dictionary of phone keys into individual characters and positions
+        /// </summary>
+        /// <param name="phoneKeys"></param>
+        /// <returns></returns>
+
+        public static Dictionary<char, int> Process(Dictionary<string, string> phoneKeys)
+        {
+            var processedKeys = new Dictionary<char, int>();
+            char[] values;
+
+            foreach (string key in phoneKeys.Keys)
+            {
+                values = phoneKeys[key].ToCharArray();
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    processedKeys.Add(values[i], i);
+                }
+            }
+
+            return processedKeys;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace GAS.TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            var phoneKeys = PhoneKeys.GetPhoneKeys();
+            var processedPhoneKeys = PhoneKeys.Process(phoneKeys);
+
+            Console.WriteLine("Enter text [a-z, *,#] or press / and enter key to end the program");
+            inputString = Console.ReadLine();
+
+            while (inputString != "/")
+            {
+                if (Validate(inputString))
+                {
+                    var minTime = GetMinInputTime(inputString, processedPhoneKeys);
+                    Console.WriteLine("Minimum time required to input given text is {0} seconds", minTime);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid characters. Please try again"); 
+                }
+
+                Console.WriteLine("Enter text [a-z, *,#] or / and enter key to end the program ");
+                inputString = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Returns minimun imput time required for a given string
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <param name="phoneKeys"></param>
+        /// <returns></returns>
+        private static decimal GetMinInputTime(string inputString, Dictionary<char, int> phoneKeys)
+        {
+            char[] inputChars =  inputString.ToCharArray();
+            decimal minTime = 0;
+            int pos;
+
+            var keyPressTime = Convert.ToDecimal(ConfigurationManager.AppSettings["KeyPressTime"]);
+            var keyPauseTime = Convert.ToDecimal(ConfigurationManager.AppSettings["KeyPauseTime"]);
+
+            foreach (char c in inputChars)
+            {
+                if (phoneKeys.ContainsKey(char.ToLower(c)))
+                {
+                    pos = phoneKeys[char.ToLower(c)];
+                    minTime = minTime + (pos * keyPressTime) + keyPauseTime;
+                } 
+            }
+
+            return minTime;
+        }
+
+        private static bool Validate(string text)
+        {
+            var r = new Regex("^[a-zA-Z0-9*#]*$");
+
+            return (text.Length > 0 && r.IsMatch(text));
+        }
+
+        
+    }
+}
